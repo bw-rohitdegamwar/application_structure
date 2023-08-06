bc8823966f40223b833b01da686b6ba8c3b942fc
bw-rohitdegamwar <rohit.degamwar@synthix.com>
2023-08-06 21:58:35 +0530

    test commit

--- data_types/main.go
+++ data_types/main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	//INT type
 	var i1 int8 = 127
 	fmt.Println(i1)
+	//Array
+	var number = [5]int{1, 2, 3, 4}
+
+	fmt.Println(number)
+
+	fmt.Printf("%T\n", number)
+
+	//slice
+	var cities = []string{"hello", "hello1"}
+	fmt.Println(cities)
+
+	//MAPS
+	var names = map[string]float64{
+		"1": 1.05,
+		"2": 2.03,
+	}
+
+	fmt.Println(names)
+
 }
