99c02bec3bb43935f1b9b3e2794638e5ae356b74
bw-rohitdegamwar <rohit.degamwar@synthix.com>
2023-08-10 09:09:37 +0530

    test

--- data_types/main.go
+++ data_types/main.go
@@ -8,11 +8,10 @@ func main() {
 	//INT type
 	var i1 int8 = 127
 	fmt.Println(i1)
+
 	//Array
 	var number = [5]int{1, 2, 3, 4}
-
 	fmt.Println(number)
-
 	fmt.Printf("%T\n", number)
 
 	//slice
@@ -24,7 +23,29 @@ func main() {
 		"1": 1.05,
 		"2": 2.03,
 	}
-
 	fmt.Println(names)
+
+	//STRUC
+	type person struct {
+		id   int
+		name string
+	}
+	var me person
+	me.id = 1
+	me.name = "rohit"
+
+	//POINTER
+	var x int = 2
+	ptr := &x
+	fmt.Printf("%T\nvalue %v\n", ptr, ptr)
+	fmt.Println(me)
+
+	//FUNCTION
+
+	fmt.Printf("%T and value %v\n", myfunc, myfunc)
+
+}
+
+func myfunc() {
 
 }
